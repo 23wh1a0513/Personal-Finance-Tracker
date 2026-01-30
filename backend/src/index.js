36ac5d7be12062e30ada58b7be50a8045e8a5a24
23wh1a0513 <23wha0513@bvrithyderabad.edu.in>
2026-01-30 12:54:22 +0530

    updated packages

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const User = require('./models/User');
+
+// const DB_URL='mongodb+srv://23wh1a0513_db_user:<db_password>@personalfinancetrackerc.qqo0fdu.mongodb.net/?appName=PersonalFinanceTrackerCluster'
+// const app=express()
+
+// app.listen(3000, ()=>{
+//     console.log("Server listening at port 3000");
+// })
+
+// app.get('/user',async()=>{
+
+// })
